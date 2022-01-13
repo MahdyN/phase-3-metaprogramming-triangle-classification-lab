d600923ffe68ad643f7ac7fe19c2f1afc6700d9d
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-01-13 17:51:24 -0500

    commit tree f271e2e0fdc83f57eeeed53235e23cc76c7906a8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
 class Triangle
-  # write code here
+  attr_reader :side_one, :side_two, :side_three
+
+  def initialize side_one, side_two, side_three
+
+      @side_one = side_one
+      @side_two = side_two
+      @side_three = side_three
+
+  end
+
+  def kind 
+    
+    raise TriangleError unless self.sides_positive? && self.no_inequality?
+
+    if @side_one == @side_two && @side_two == @side_three
+        :equilateral
+    elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
+        :isosceles
+    else
+        :scalene
+    end
+
+  end
+
+  def sides_positive?
+
+    [@side_one, @side_two, @side_three].all? { |side| side > 0 }
+
+  end
+
+  def no_inequality?
+
+      @side_one + @side_two > @side_three && @side_two + @side_three > @side_one && @side_one + @side_three > @side_two
+
+  end
+
+class TriangleError < StandardError
+
+    def message
+        "Entered Triangle Sides Are Not Valid"
+    end
+
+end
+
+
+
 end
